8b1aeb5109abf3baeb06ce1d3ee3a08946889f55
one-west <wjdgkstj112@naver.com>
2025-05-23 14:22:13 +0900

    refactor: 폰트 설정용 유틸

--- startup_font.py
+++ startup_font.py
@@ -3,16 +3,21 @@ import matplotlib.font_manager as fm
 import os
 import requests
 
-
 def set_korean_font():
-    font_path = "/tmp/NanumGothic.ttf"
+    if os.name == 'nt':  # Windows
+        font_path = os.path.join(os.getcwd(), "NanumGothic.ttf")
+    else:
+        font_path = "/tmp/NanumGothic.ttf"
+
     if not os.path.exists(font_path):
-        url = "https://github.com/naver/nanumfont/raw/master/ttf/NanumGothic.ttf"
+        url = "https://github.com/naver/nanumfont/releases/download/VER2.5/NanumGothic.ttf"
         r = requests.get(url)
         with open(font_path, "wb") as f:
             f.write(r.content)
 
-    # ✅ 폰트 등록
+    if not font_path.endswith(".ttf") or os.path.getsize(font_path) < 10000:
+        raise RuntimeError("폰트 다운로드 실패 또는 잘못된 파일 형식입니다.")
+
     fm.fontManager.addfont(font_path)
     plt.rcParams["font.family"] = "NanumGothic"
     plt.rcParams["axes.unicode_minus"] = False
