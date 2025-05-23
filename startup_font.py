610757aba923a9acec34e29629e3fd7011737a4d
one-west <wjdgkstj112@naver.com>
2025-05-23 14:25:28 +0900

    refactor: 폰트 설정용 유틸

--- startup_font.py
+++ startup_font.py
@@ -1,23 +1,16 @@
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 import os
-import requests
 
 def set_korean_font():
-    if os.name == 'nt':  # Windows
-        font_path = os.path.join(os.getcwd(), "NanumGothic.ttf")
-    else:
-        font_path = "/tmp/NanumGothic.ttf"
-
+    # 폰트 파일 경로 설정
+    font_path = os.path.join(os.getcwd(), "NanumGothic-Regular.ttf")
+    
+    # 폰트 파일이 존재하는지 확인
     if not os.path.exists(font_path):
-        url = "https://github.com/naver/nanumfont/releases/download/VER2.5/NanumGothic.ttf"
-        r = requests.get(url)
-        with open(font_path, "wb") as f:
-            f.write(r.content)
-
-    if not font_path.endswith(".ttf") or os.path.getsize(font_path) < 10000:
-        raise RuntimeError("폰트 다운로드 실패 또는 잘못된 파일 형식입니다.")
-
+        raise FileNotFoundError(f"폰트 파일이 {font_path}에 존재하지 않습니다. 다운로드 후 해당 위치에 저장해주세요.")
+    
+    # 폰트 등록
     fm.fontManager.addfont(font_path)
     plt.rcParams["font.family"] = "NanumGothic"
     plt.rcParams["axes.unicode_minus"] = False
