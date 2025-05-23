c91521a454eb7165d56ea957e545bf7a487e997f
one-west <wjdgkstj112@naver.com>
2025-05-23 14:14:56 +0900

    refactor: 폰트 설정용 유틸

--- startup_font.py
+++ startup_font.py
@@ -4,7 +4,6 @@ import os
 import requests
 
 
-# 나눔고딕 다운로드 및 등록 (최초 실행 시)
 def set_korean_font():
     font_path = "/tmp/NanumGothic.ttf"
     if not os.path.exists(font_path):
@@ -13,7 +12,7 @@ def set_korean_font():
         with open(font_path, "wb") as f:
             f.write(r.content)
 
-    # 폰트 설정
+    # ✅ 폰트 등록
     fm.fontManager.addfont(font_path)
     plt.rcParams["font.family"] = "NanumGothic"
     plt.rcParams["axes.unicode_minus"] = False
